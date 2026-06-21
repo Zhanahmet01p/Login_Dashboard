a4090f84e1a78a1f6588a7bd60fc081e703f6822
Zhanahmet01p <kzhanahmet573@gmail.com>
2026-06-21 20:53:45 +0500

    new backend

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,25 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-});
+const config = process.env.DATABASE_URL
+  ? {
+    
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false, 
+      },
+    }
+  : {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_DATABASE,
+      ssl: process.env.DB_HOST !== '127.0.0.1' && process.env.DB_HOST !== 'localhost'
+        ? { rejectUnauthorized: false }
+        : false,
+    };
+
+const pool = new Pool(config);
 
 module.exports = pool;
